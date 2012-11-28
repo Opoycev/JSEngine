e813de7e5c00ab8b9ed952ced765b3e6d8df5fc9
Kirill Muratov <kirill.muratov@outlook.com>
2012-11-28 16:45:09 +0400

    Code format

--- Constants.js
+++ Constants.js
@@ -1,44 +1,51 @@
-var Constants = new Object();
+var Constants = {
+    //Число пи
+    Pi: 3.141592654,
+    Pi2: 6.283185307,
+    PiDiv2: 1.570796327,
+    PiDiv4: 0.785398163,
 
-//Число пи
-Constants.Pi = 3.141592654;
-Constants.Pi2 = 6.283185307;
-Constants.PiDiv2 = 1.570796327;
-Constants.PiDiv4 = 0.785398163;
+    //Константы, связанные с числами с фиксированной точкой
+    FIXP16_SHIFT: 16,
+    FIXP16_MAG: 65536,
+    FIXP16_DP_MASK: 0x0000ffff,
+    FIXP16_WP_MASK: 0xffff0000,
+    FIXP16_ROUND_UP: 0x00008000,
 
-//Константы, связанные с числами с фиксированной точкой
-Constants.FIXP16_SHIFT = 16;
-Constants.FIXP16_MAG = 65536;
-Constants.FIXP16_DP_MASK = 0x0000ffff;
-Constants.FIXP16_WP_MASK = 0xffff0000;
-Constants.FIXP16_ROUND_UP = 0x00008000;
+    //Малые числа (сравнения)
+    Epsilon_E4: 1E-4,
+    Epsilon_E5: 1E-5,
+    Epsilon_E6: 1E-6,
 
-//Малые числа (сравнения)
-Constants.Epsilon_E4 = 1E-4;
-Constants.Epsilon_E5 = 1E-5;
-Constants.Epsilon_E6 = 1E-6;
+    //Параметры пересечения отрезков
+    ParamLineNoIntersect: 0,
+    ParamLineIntersectInSegment: 1,
+    ParamLineIntersectOutSegment: 2,
+    ParamLineIntersectEverywhere: 3,
 
-//Параметры пересечения отрезков
-Constants.ParamLineNoIntersect = 0;
-Constants.ParamLineIntersectInSegment = 1;
-Constants.ParamLineIntersectOutSegment = 2;
-Constants.ParamLineIntersectEverywhere = 3;
+    //Единичные матрицы
+    Matrix4x4Imat: {
+        M: [[1, 0, 0, 0],
+            [0, 1, 0, 0],
+            [0, 0, 1, 0],
+            [0, 0, 0, 1]]
+    },
 
-//Единичные матрицы
-Constants.Matrix4x4Imat = new Object();
-Constants.Matrix4x4Imat.M = [[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]];
+    Matrix4x3ImatMathIncorrect: {
+        M: [[1, 0, 0],
+            [0, 1, 0],
+            [0, 0, 1],
+            [0, 0, 0]]
+    },
 
-Constants.Matrix4x3ImatMathIncorrect = new Object();
-Constants.Matrix4x3ImatMathIncorrect.M = [[1, 0, 0], [0, 1, 0], [0, 0, 1], [0, 0, 0]];
+    Matrix3x3Imat: {
+        M: [[1, 0, 0],
+            [0, 1, 0],
+            [0, 0, 1]]
+    },
 
-
-Constants.Matrix3x3Imat = new Object();
-Constants.Matrix3x3Imat.M = [[1, 0, 0], [0, 1, 0], [0, 0, 1]];
-
-Constants.Matrix2x2Imat = new Object();
-Constants.Matrix2x2Imat.M = [[1, 0], [0, 1]];
-
-
-
-
-
+    Matrix2x2Imat: {
+        M: [[1, 0],
+            [0, 1]]
+    }
+}
